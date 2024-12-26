8bfd1f6a5abaf74a5bfde3450f40554fc39798f9
LakshyaMittal11 <lakshyamittal2405@gmail.com>
2024-12-26 13:38:01 +0530

    update

--- router.js
+++ router.js
@@ -20,26 +20,26 @@ con.connect(function(err){
 /*-------------------------------------*/
 
 
-app.get("/",function(req,res)
+
+app.get("/login",function(req,res)
 {
-res.sendFile("./bookshop/html/index.html",{root:__dirname});
+res.sendFile("./bookshop/html/login.html",{root:__dirname});
 });
 app.get("/index",function(req,res)
 {
 res.sendFile("./bookshop/html/index.html",{root:__dirname});
 });
-app.get("/login",function(req,res)
-{
-res.sendFile("./bookshop/html/login.html",{root:__dirname});
-});
+
 app.get("/home",function(req,res)
 {
 res.sendFile("./bookshop/html/home.html",{root:__dirname});
 });
+
 app.get("/contact",function(req,res)
 {
 res.sendFile("./bookshop/html/contact.html",{root:__dirname});
 });
+
 app.get("/about",function(req,res)
 {
 res.sendFile("./bookshop/html/about.html",{root:__dirname});
@@ -52,15 +52,22 @@ app.get("/Register",function(req,res)
 {
 res.sendFile("./bookshop/html/register.html",{root:__dirname});
 });
+
 var bd=require('body-parser');
 var ed=bd.urlencoded({extended:false}) 
 app.post("/Contactprocess",ed,function(req,res)
 {
+    var d=req.body.N;
     var a=req.body.E;
     var b=req.body.P;
-    var c=req.body.A;
-    var submit="email="+a+"phone number="+b+"address="+c;
-res.send(submit);
+    var c=req.body.M;
+    var q="insert into contact values('"+d+"','"+a+"','"+b+"','"+c+"')";
+   con.query(q,function(err,result)
+   {
+    if(err)
+        throw err;
+    res.send("successfully send");
+});
 });
 
 app.post("/regprocess",ed,function(req,res)
@@ -78,11 +85,32 @@ app.post("/regprocess",ed,function(req,res)
 });
 
 
-app.get("/loginprocess",function(req,res){
-var a=req.query.E;
-var b=req.query.P;
-var login="email="+a+"password="+b;
-res.send(login);
+app.post("/loginprocess",ed,function(req,res){
+var a=req.body.E;
+var b=req.body.P;
+console.log(a);
+console.log(b);
+var q="select * from users where email='"+a+"'";
+con.query(q,function(err,result){
+    if(err)
+        throw err;
+    console.log(result);
+    var L=result.length;
+    if(L>0){
+        var p=result[0].pwd;
+        if(p==b)
+            res.send("valid user");
+        else
+        res.send("Password is invalid");
+    }
+    else
+    res.send("Email is invalid");
+});
+    
 });
 
-app.listen(3000);
+
+app.listen(5000,function(req,res)
+{
+console.log("Project run on port no 5000");
+});
